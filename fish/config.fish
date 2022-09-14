1aa9018a74ae6b89ae6fc7b50469de7da4222f89
devinlinux <102993661+devinlinux@users.noreply.github.com>
2022-09-13 22:49:07 -0400

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,16 @@
+set fish_greeting ""
+
+set -gx TERM xterm-256color
+
+# theme
+set -g theme_color_scheme terminal-dark
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
+
+# aliases
+alias lsa "ls -A"
+alias lla "ls -A -l"
+alias g "git"
+alias gc "git clone"
